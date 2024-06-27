f560d7f04a3a44f61bef1816cf0caf9d2662878e
Muhammad Ammar <syedammarzaki4@gmail.com>
2024-06-27 14:46:31 +0500

    .

--- muhammad_ammar/lib/widgets/header.dart
+++ muhammad_ammar/lib/widgets/header.dart
@@ -9,73 +9,103 @@ class Header extends StatelessWidget {
     return Center(
       child: Padding(
         padding: const EdgeInsets.only(top: 18),
-        child: Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: Colors.white,),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: Colors.white,
+          ),
           height: MediaQuery.of(context).size.height * 0.061,
           width: MediaQuery.of(context).size.width * 0.63,
-         
           child: Padding(
-            padding: const EdgeInsets.only(left: 5,right: 5),
+            padding: const EdgeInsets.only(left: 5, right: 5),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const Text("ryze.",style: TextStyle(color: Colors.black),),
-                const Expanded(
+                Image.asset(
+                  "assets/images/ryze.png",
+                  width: 120,
+                  fit: BoxFit.fill,
+                  filterQuality: FilterQuality.high,
+                ),
+                Expanded(
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 18.0),
-                        child: Text(
-                          'HOME',
-                          style: TextStyle(
-                              fontFamily: 'Sora', fontWeight: FontWeight.w600,fontSize: 12,color: Colors.black87),
+                        padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                        child: TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'HOME',
+                            style: TextStyle(
+                                fontSize: 13,
+                                fontFamily: 'Sora',
+                                fontWeight: FontWeight.w600,
+                                color: Colors.black87),
+                          ),
                         ),
                       ),
-                      Padding(
+                      const Padding(
                         padding: EdgeInsets.symmetric(horizontal: 18.0),
                         child: Text(
                           'ABOUTME',
-                          style: TextStyle(fontSize: 13,
-                              fontFamily: 'Sora', fontWeight: FontWeight.w600,color: Colors.black87),
+                          style: TextStyle(
+                              fontSize: 13,
+                              fontFamily: 'Sora',
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black87),
                         ),
                       ),
-                      Padding(
+                      const Padding(
                         padding: EdgeInsets.symmetric(horizontal: 18.0),
                         child: Text(
                           'PORTFOLIO',
                           style: TextStyle(
-                              fontFamily: 'Sora', fontWeight: FontWeight.w600,fontSize: 12,color: Colors.black87),
+                              fontFamily: 'Sora',
+                              fontWeight: FontWeight.w600,
+                              fontSize: 12,
+                              color: Colors.black87),
                         ),
                       ),
-                      Padding(
+                      const Padding(
                         padding: EdgeInsets.symmetric(horizontal: 18.0),
                         child: Text(
                           'SERVICES',
                           style: TextStyle(
-                              fontFamily: 'Sora', fontWeight: FontWeight.w600,fontSize: 12,color: Colors.black87),
+                              fontFamily: 'Sora',
+                              fontWeight: FontWeight.w600,
+                              fontSize: 12,
+                              color: Colors.black87),
                         ),
                       ),
-                      Padding(
+                      const Padding(
                         padding: EdgeInsets.symmetric(horizontal: 18.0),
                         child: Text(
                           'CONTACT',
                           style: TextStyle(
-                              fontFamily: 'Sora', fontWeight: FontWeight.w600,fontSize: 12,color: Colors.black87),
+                              fontFamily: 'Sora',
+                              fontWeight: FontWeight.w600,
+                              fontSize: 12,
+                              color: Colors.black87),
                         ),
                       ),
                     ],
                   ),
                 ),
-                ShadButton(hoverBackgroundColor: Colors.orangeAccent[900],
+                ShadButton(
+                  hoverBackgroundColor: Colors.orange,
+                  cursor: SystemMouseCursors.click,
+                  pressedBackgroundColor: Colors.orange,
                   backgroundColor: Colors.black,
                   width: 120,
                   text: const Text(
                     "Let's Talk",
                     style: TextStyle(
-                        fontFamily: 'Sora',
-                        fontWeight: FontWeight.w400,
-                        color: Colors.white,
-                        fontSize: 12),
+                      fontFamily: 'Sora',
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                      fontSize: 12,
+                    ),
                   ),
                   onPressed: () {},
                 ),
