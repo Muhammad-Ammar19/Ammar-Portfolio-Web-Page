0017630205b8e784dee81014fe78c220bae81d8b
Muhammad Ammar <syedammarzaki4@gmail.com>
2024-07-02 19:41:08 +0500

    .

--- muhammad_ammar/lib/widgets/education_section.dart
+++ muhammad_ammar/lib/widgets/education_section.dart
@@ -22,7 +22,7 @@ class EducationSection extends StatelessWidget {
           TextButton.icon(
             onPressed: () {},
             label: const Text(
-              "Education",
+              "My Education",
               style: TextStyle(
                   color: Colors.black,
                   fontFamily: 'Sora',
@@ -31,134 +31,152 @@ class EducationSection extends StatelessWidget {
             ),
             icon: const Icon(
               size: 35,
-              Icons.work_outlined,
+              Icons.book_rounded,
               color: Colors.black,
             ),
           ),
-          Container(
-              decoration: const BoxDecoration(color: Colors.transparent),
-              width: MediaQuery.of(context).size.width * 0.25,
-              child: const Padding(
-                padding: EdgeInsets.all(15.0),
-                child: Column(children: [
-                  Row(
-                    children: [
-                      Text(
-                        '2018 - 2024',
-                        style: TextStyle(
-                            fontFamily: 'Sora',
-                            fontSize: 15,
-                            color: Color(0xFFEA552B),
-                            fontWeight: FontWeight.w600),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 55.0),
-                        child: Column(
-                          children: [
-                            Text(
-                              "Bachelor's in Computer Science",
-                              style: TextStyle(
-                                  fontFamily: 'Sora',
-                                  color: Colors.black,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w500),
-                            ),
-                            Text(
-                              'Alhamd Islamic University',
-                              style: TextStyle(
-                                  fontFamily: 'Sora',
-                                  color: Colors.black,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w300),
-                            ),
-                          ],
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.transparent,
+            border: Border.all(
+              color: Colors.transparent,
+              width: 1,
+            ),
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                spreadRadius: 1,
+                blurRadius: 1,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          width: MediaQuery.of(context).size.width * 0.25,
+          child: const Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Column(children: [
+              Row(
+                children: [
+                  Text(
+                    '2018 - 2024',
+                    style: TextStyle(
+                        fontFamily: 'Sora',
+                        fontSize: 15,
+                        color: Color(0xFFEA552B),
+                        fontWeight: FontWeight.w600),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 55.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Bachelor's in Computer Science",
+                          style: TextStyle(
+                              fontFamily: 'Sora',
+                              color: Colors.black,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500),
                         ),
-                      ),
-                    ],
-                  ),
-                  Divider(
-                      color: Color.fromARGB(131, 255, 255, 255),
-                    height: 1,
-                  
-                    thickness: 1,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        '2018 - 2020',
-                        style: TextStyle(
-                            fontFamily: 'Sora',
-                            fontSize: 15,
-                            color: Color(0xFFEA552B),
-                            fontWeight: FontWeight.w600),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 55),
-                        child: Column(
-                          children: [
-                            Text(
-                              "Fsc Pre-Engineering",
-                              style: TextStyle(
-                                  fontFamily: 'Sora',
-                                  color: Colors.black,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w500),
-                            ),
-                            Text(
-                              'Govt Boys College',
-                              style: TextStyle(
-                                  fontFamily: 'Sora',
-                                  fontSize: 18,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w300),
-                            ),
-                          ],
+                        Text(
+                          'Alhamd Islamic University',
+                          style: TextStyle(
+                              fontFamily: 'Sora',
+                              color: Colors.black,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w300),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                  Divider(
-                    color: Color.fromARGB(131, 255, 255, 255),
-                    height: 1,
-                    thickness: 1,
+                ],
+              ),
+              Divider(
+                color: Color.fromARGB(131, 255, 255, 255),
+                height: 20,
+                thickness: 1,
+              ),
+              Row(
+                children: [
+                  Text(
+                    '2018 - 2020',
+                    style: TextStyle(
+                        fontFamily: 'Sora',
+                        fontSize: 15,
+                        color: Color(0xFFEA552B),
+                        fontWeight: FontWeight.w600),
                   ),
-                  Row(
-                    children: [
-                      Text(
-                        '2006 - 2018',
-                        style: TextStyle(
-                            fontFamily: 'Sora',
-                            fontSize: 15,
-                            color: Color(0xFFEA552B),
-                            fontWeight: FontWeight.w600),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 55),
-                        child: Column(
-                          children: [
-                            Text(
-                              "Matriculation",
-                              style: TextStyle(
-                                  fontFamily: 'Sora',
-                                  color: Colors.black,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w500),
-                            ),
-                            Text(
-                              'OPF Public School',
-                              style: TextStyle(
-                                  fontFamily: 'Sora',
-                                  fontSize: 15,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w500),
-                            ),
-                          ],
+                  Padding(
+                    padding: EdgeInsets.only(left: 55),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Fsc Pre-Engineering",
+                          style: TextStyle(
+                              fontFamily: 'Sora',
+                              color: Colors.black,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500),
                         ),
-                      ),
-                    ],
-                  )
-                ]),
-              )),
+                        Text(
+                          'Govt Boys College',
+                          style: TextStyle(
+                              fontFamily: 'Sora',
+                              fontSize: 18,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w300),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Divider(
+                color: Color.fromARGB(131, 255, 255, 255),
+                height: 20,
+                thickness: 1,
+              ),
+              Row(
+                children: [
+                  Text(
+                    '2006 - 2018',
+                    style: TextStyle(
+                        fontFamily: 'Sora',
+                        fontSize: 15,
+                        color: Color(0xFFEA552B),
+                        fontWeight: FontWeight.w600),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 55),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Matriculation",
+                          style: TextStyle(
+                              fontFamily: 'Sora',
+                              color: Colors.black,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500),
+                        ),
+                        Text(
+                          'OPF Public School',
+                          style: TextStyle(
+                              fontFamily: 'Sora',
+                              fontSize: 15,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ]),
+          ),
+        ),
         ],
       )
     ]);
