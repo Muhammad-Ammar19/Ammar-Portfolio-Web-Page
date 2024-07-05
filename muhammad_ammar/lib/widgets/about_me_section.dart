1ddeec6da65e295224abad13282ea7a3c74c8a94
Muhammad Ammar <syedammarzaki4@gmail.com>
2024-07-05 21:38:53 +0500

    .

--- muhammad_ammar/lib/widgets/about_me_section.dart
+++ muhammad_ammar/lib/widgets/about_me_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 
 class AboutMeSection extends StatelessWidget {
   const AboutMeSection({super.key});
@@ -8,23 +9,129 @@ class AboutMeSection extends StatelessWidget {
     return SizedBox(
       height: MediaQuery.of(context).size.height * 0.8,
       width: double.infinity,
-      child: Stack(fit: StackFit.expand,
-        children:[ Image.network(
+      child: Stack(fit: StackFit.expand, children: [
+        Image.network(
           "https://petrix-react.vercel.app/images/golden_bg.jpg",
           fit: BoxFit.cover,
         ),
-        const Center(child: Row(
-          children: [
-            Column(
-              children: [
-              
-              ],
-            ),
-          ],
-        )),
-        
-        ]
-      ),
+        Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Image(
+                image:
+                    AssetImage("assets/images/muhammad-ammar-bg-removed.png"),
+              ),
+              const SizedBox(width: 20),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.network(
+                    "https://petrix-react.vercel.app/_next/static/media/about_shapes.df78a495.png",
+                    height: 40,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 12),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      child: const Text(
+                        "Hello! I am Muhammad Ammar, Full Stack Flutter Developer from Quetta. I have extensive experience in both app and web development and specialize in creating smooth and responsive applications. I am also familiar with web technologies. I enjoy discussing and working on unique projects that bring innovative solutions to life.",
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black,
+                          fontFamily: 'Sora',
+                          fontWeight: FontWeight.w600,
+                          height: 1.5, 
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                      height: 15), 
+                  const Row(
+                    children: [
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                "12+",
+                                style: TextStyle(
+                                  fontSize: 45,
+                                  color: Colors.black,
+                                  fontFamily: 'Sora',
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            Text(
+                            "Years Of \nExperience",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                              fontFamily: 'Sora',fontWeight: FontWeight.w700
+                            ),
+                          ),  ],
+                          ),
+                        
+                        ],
+                      ),
+                      SizedBox(width: 25),
+                      Column(
+                        children: [
+                          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "12+",
+                                style: TextStyle(
+                                  fontSize: 45,
+                                  color: Colors.black,
+                                  fontFamily: 'Sora',
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                          Text(
+                            "Successful \nProjects",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                              fontFamily: 'Sora',fontWeight: FontWeight.w700
+                            ),
+                          ),   ],
+                          ),
+                         
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                      height: 15), 
+                  ShadButton(decoration: const ShadDecoration(shadows: ShadShadows.sm),
+                    hoverBackgroundColor: const Color(0xFFEA552B),
+                    cursor: SystemMouseCursors.click,
+                    pressedBackgroundColor: const Color(0xFFEA552B),
+                    backgroundColor: Colors.black,
+                    width: 160,
+                    height: 55,
+                    text: const Text(
+                      "DOWNLOAD CV",
+                      style: TextStyle(
+                        fontFamily: 'Sora',
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                        fontSize: 14,
+                      ),
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ]),
     );
   }
 }
