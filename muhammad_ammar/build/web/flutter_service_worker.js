00125981559d0a99374e38fe75f041e3785746eb
Muhammad Ammar <syedammarzaki4@gmail.com>
2024-07-16 22:25:36 +0500

    .

--- muhammad_ammar/build/web/flutter_service_worker.js
+++ muhammad_ammar/build/web/flutter_service_worker.js
@@ -66,10 +66,10 @@ const RESOURCES = {"assets/AssetManifest.bin": "85627e32811428c56cea0f8f1d53d008
 "favicon_io/favicon-32x32.png": "a359635323b9e0edf5b30fb3c85c207f",
 "favicon_io/site.webmanifest": "053100cb84a50d2ae7f5492f7dd7f25e",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "12a81147162d01180f0c7ab534098a0f",
+"flutter_bootstrap.js": "babf632497f0914a27780c9c61c736f3",
 "index.html": "68e912e2f3d3a0698d52f6fbd272fcfa",
 "/": "68e912e2f3d3a0698d52f6fbd272fcfa",
-"main.dart.js": "764b3349f91ca13a3ec2b87fa484286c",
+"main.dart.js": "4955920cede04ea11ff2eb90febb9858",
 "manifest.json": "e65ff52af0a583c4a4c6e2a5fdd5dec5",
 "version.json": "bcdcb627e618b231d9d83a2ae5b89ea2"};
 // The application shell files that are downloaded before a service worker can
