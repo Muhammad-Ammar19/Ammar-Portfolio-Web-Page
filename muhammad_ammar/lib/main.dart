288be7cfa1254a805f2b96277894ac0b8bf54d5d
Muhammad Ammar <syedammarzaki4@gmail.com>
2024-07-30 21:30:14 +0500

    loading screen added

--- muhammad_ammar/lib/main.dart
+++ muhammad_ammar/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:muhammad_ammar/controller/get_controller.dart';
@@ -5,8 +7,8 @@ import 'package:muhammad_ammar/responsive/mobile_scaffold.dart';
 import 'package:muhammad_ammar/responsive/responsive_layout.dart';
 import 'package:muhammad_ammar/responsive/tablet_scaffold.dart';
 import 'package:muhammad_ammar/responsive/desktop_scaffold.dart';
+import 'package:muhammad_ammar/widgets/loading.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -28,17 +30,58 @@ class MyApp extends StatelessWidget {
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  Future<void> prefetchImages() async {
+    
+    List<String> imageUrls = [
+      'https://petrix-react.vercel.app/_next/static/media/body_bg.255c616a.jpg',
+      'https://petrix-react.vercel.app/images/golden_bg.jpg',
+      
+    ];
+
+    await Future.wait(imageUrls.map((url) => _loadNetworkImage(url)));
+  }
+
+  Future<void> _loadNetworkImage(String url) async {
+    final Completer<void> completer = Completer();
+    final Image image = Image.network(url);
+
+    image.image.resolve(const ImageConfiguration()).addListener(
+      ImageStreamListener((_, __) {
+        completer.complete();
+      }, onError: (dynamic exception, StackTrace? stackTrace) {
+        completer.complete();
+      }),
+    );
+
+    return completer.future;
+  }
+
   @override
   Widget build(BuildContext context) {
     return GetBuilder<GetController>(
       init: GetController(),
       builder: (controller) {
-        return  ShadApp.material(debugShowCheckedModeBanner: false,
-          home: ResponsiveLayout(
-            mobileScaffold: MobileScaffold(),
-            tabletScaffold: const TabletScaffold(),
-            desktopScaffold: const DesktopScaffold(),
-          ),
+        return FutureBuilder(
+          future: prefetchImages(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              
+              return const Scaffold(
+                body: Center(
+                  child: LoadingScreen(),
+                ),
+              );
+            } else {
+              return ShadApp.material(
+                debugShowCheckedModeBanner: false,
+                home: ResponsiveLayout(
+                  mobileScaffold: MobileScaffold(),
+                  tabletScaffold: const TabletScaffold(),
+                  desktopScaffold: const DesktopScaffold(),
+                ),
+              );
+            }
+          },
         );
       },
     );
--- muhammad_ammar/lib/widgets/loading.dart
+++ muhammad_ammar/lib/widgets/loading.dart
@@ -1,10 +1,76 @@
 import 'package:flutter/material.dart';
 
-class LoadingScreen extends StatelessWidget {
+class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
 
   @override
+  LoadingScreenState createState() => LoadingScreenState();
+}
+
+class LoadingScreenState extends State<LoadingScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _animation = Tween<double>(begin: -50, end: 250).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: CircularProgressIndicator());
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(234, 0, 0, 0),
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            // Animated Ball
+            AnimatedBuilder(
+              animation: _animation,
+              builder: (context, child) {
+                return Positioned(
+                  left: _animation.value,
+                  top: 0,
+                  bottom: 0,
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: const BoxDecoration(
+                     color:  Color(0xFFEA552B),
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                );
+              },
+            ),
+           
+            const Text(
+              "Loading",
+              style:  TextStyle(
+                fontFamily: 'Sora',
+                fontSize: 38,
+                fontWeight: FontWeight.w700,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
